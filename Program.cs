fd0efbc3891029190f5c45d9812f09b772355bcf
OmarHamdyx <zoltarscorpio@gmail.com>
2024-02-12 21:04:54 +0200

    Test

--- Program.cs
+++ Program.cs
@@ -37,6 +37,8 @@ app.Run(async (HttpContext context) =>
                     result = num1 * num2;
                     break;
                 case "%":
+                    result = num1 % num2;
+                    break;
                 case "/":
                     if (num2 != 0)
                         result = num1 / num2;
